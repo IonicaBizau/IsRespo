7d391af71d78207962e1cb8794139c87936b4295
Ionică Bizău <bizauionica@yahoo.com>
2014-03-28 06:51:28 +0200

    Compute iframe url from search query

---
+++ functions.js
@@ -0,0 +1,8 @@
+function queryString (name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+$("iframe").attr("src", queryString("site"));
