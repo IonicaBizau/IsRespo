74ffe26f87827ea4290ce0bf10c2df2b1521ff12
Ionică Bizău <bizauionica@yahoo.com>
2014-03-28 06:50:51 +0200

    Commented html2canvas and added tablet view

--- functions.js
+++ functions.js
@@ -1,12 +1,7 @@
-$("button").on("click", function () {
-    var $this = $(this);
-   // $this.hide();
-    html2canvas(document.body, {
-      onrendered: function(canvas) {
-            var imageLink = canvas.toDataURL();
-            window.open(imageLink);
-          console.log(new Date(), imageLink);
-          $this.show();
-      }
-    });
+//html2canvas(document.body, {
+  onrendered: function(canvas) {
+        var imageLink = canvas.toDataURL();
+        window.open(imageLink);
+      console.log(imageLink);
+  }
 });
