46695fad449271a3f8dd1cb1f274a2a732b1a435
Ionică Bizău <bizauionica@yahoo.com>
2014-03-28 06:50:26 +0200

    Render canvas on button click

--- functions.js
+++ functions.js
@@ -1,7 +1,12 @@
-html2canvas(document.body, {
-  onrendered: function(canvas) {
-        var imageLink = canvas.toDataURL();
-        window.open(imageLink);
-      console.log(imageLink);
-  }
+$("button").on("click", function () {
+    var $this = $(this);
+    $this.hide();
+    html2canvas(document.body, {
+      onrendered: function(canvas) {
+            var imageLink = canvas.toDataURL();
+            window.open(imageLink);
+          console.log(new Date(), imageLink);
+          $this.show();
+      }
+    });
 });
