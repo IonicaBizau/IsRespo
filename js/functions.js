63339cc1bee631f4e74cdac436968db06638c5ba
Ionică Bizău <bizauionica@yahoo.com>
2014-04-30 14:11:41 +0300

    Moved code inside of ready handler

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,19 @@
-function queryString (name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-
-$("iframe").attr("src", queryString("site"));
-
-$(".url-to-load").on("keyup", function (e) {
-    if (e.keyCode === 13) {
-        $("iframe").attr("src", $(this).val());
-    }
-});
-
-
 $(document).ready(function() {
+
+    function queryString (name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+            results = regex.exec(location.search);
+        return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
+
+    $("iframe").attr("src", queryString("site"));
+
+    $(".url-to-load").on("keyup", function (e) {
+        if (e.keyCode === 13) {
+            $("iframe").attr("src", $(this).val());
+        }
+    });
 
     $.fn.fullpage({
         "verticalCentered": false
