6fd587e5e9c5c264603b0e093d75efd7eb61652a
Ionică Bizău <bizauionica@yahoo.com>
2014-05-25 10:38:00 +0300

    Fixed #5 - Handle local files
    Don't prepend "http" (#2) if the provided url is a local file.

--- js/functions.js
+++ js/functions.js
@@ -10,18 +10,17 @@ $(document).ready(function() {
     $("iframe").attr("src", queryString("site"));
 
     $(".url-to-load").on("keyup", function (e) {
-        if (e.keyCode === 13) {
-            var url = $(this).val().replace(/^\W+/, "");
+        if (e.keyCode !== 13) { return; }
+        var url = $(this).val().replace(/^\W+/, "");
 
-            /*
-             * Append protocol to domain if it wasn't provided
-             * Use HTTP protocol by default. It will switch to HTTPS if web server will redirect to it
-             */
-            if (!url.match(/^http/i)) {
-                url = "http://" + url;
-            }
-
-            $("iframe").attr("src", url);
+        /*
+         * Append protocol to domain if it wasn't provided
+         * Use HTTP protocol by default. It will switch to HTTPS if web server will redirect to it
+         */
+        if (!/^http/i.test(url) && !/^file:/i.test(url)) {
+            $(this).val(url = "http://" + url);
         }
+
+        $("iframe").attr("src", url);
     });
 });
