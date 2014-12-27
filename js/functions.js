d3c9a075589373f88f4d7d346d34ed8c13eda117
Ionică Bizău <bizauionica@yahoo.com>
2014-12-27 17:43:58 +0200

    Fixed #3. Handle x-frame-options header and display an error page.

--- js/functions.js
+++ js/functions.js
@@ -2,25 +2,40 @@ $(document).ready(function() {
 
     function queryString (name) {
         name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-            results = regex.exec(location.search);
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)")
+          , results = regex.exec(location.search)
+          ;
         return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
     }
 
-    $("iframe").attr("src", queryString("site"));
+    var $iframe = $("iframe")
+      , src = queryString("site") || "demo.html"
+      , url = null
+      ;
 
+    // Default url to load
+    $iframe.attr("src", src);
+
+    // Write the url
     $(".url-to-load").on("keyup", function (e) {
         if (e.keyCode !== 13) { return; }
-        var url = $(this).val().replace(/^\W+/, "");
-
-        /*
-         * Append protocol to domain if it wasn't provided
-         * Use HTTP protocol by default. It will switch to HTTPS if web server will redirect to it
-         */
+        url = $(this).val().replace(/^\W+/, "");
         if (!/^http/i.test(url) && !/^file:/i.test(url)) {
             $(this).val(url = "http://" + url);
         }
 
-        $("iframe").attr("src", url);
+        // Check if we can load it into an iframe
+        $.ajax({
+            url: "http://urlreq.appspot.com/req?method=GET&url=" + encodeURIComponent(url) + "&debugMode=1"
+          , success: function (res) {
+                if (!/x-frame-options: (sameorigin|deny)/i.test(res)) {
+                    return;
+                }
+                $iframe.attr("src", "not-allowed.html");
+            }
+        });
+
+        $iframe.attr("src", url);
     });
+
 });
